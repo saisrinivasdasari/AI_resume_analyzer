4dff19a472ce9b53deb5335ce78a8f77f524109b
saisrinivasdasari <saisrinivasdasari2000@gmail.com>
2025-07-28 15:41:12 +0530

    authentication

--- vite-project/app/routes.ts
+++ vite-project/app/routes.ts
@@ -1,3 +1,17 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+// import { type RouteConfig, index } from "@react-router/dev/routes";
+// import path from "path";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+// export default [index("routes/home.tsx"),
+//     route(path:'/auth',file:'routes/auth.tsx')
+// ] satisfies RouteConfig;
+
+import { type RouteConfig, index } from "@react-router/dev/routes";
+import path from "path";
+
+export default [
+    index("routes/home.tsx"),
+    {
+        path: '/auth',
+        file: 'routes/auth.tsx'
+    }
+] satisfies RouteConfig;
